16c34c35207b75957a1ad6e5049415cdcbea17ba
SalaheddineBelfirme <belfimesalaheddine@gmail.com>
2024-02-08 16:04:42 +0100

    profile creation for each applicant

--- src/main/java/mhkif/yc/myrh/model/JobSeeker.java
+++ src/main/java/mhkif/yc/myrh/model/JobSeeker.java
@@ -1,5 +1,6 @@
 package mhkif.yc.myrh.model;
 
+import lombok.Builder;
 import mhkif.yc.myrh.enums.RoleEnum;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -16,10 +17,12 @@ public class JobSeeker extends Person{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-
     @Enumerated(EnumType.STRING)
     private RoleEnum role = RoleEnum.APPLICANT;
+    @ManyToOne
+    private  Profile profile;
 
+    private Boolean profile_verify;
     // The entity that specifies the @JoinTable is the owning side of the relationship and
     // the entity that specifies the mappedBy attribute is the inverse side.
     @ManyToMany(fetch = FetchType.LAZY,
@@ -29,6 +32,8 @@ public class JobSeeker extends Person{
             },
             mappedBy = "jobSeekers"
     )
+
    Set<Offer> offers = new HashSet<>();
+
 
 }
