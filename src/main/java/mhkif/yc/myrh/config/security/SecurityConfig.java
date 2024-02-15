c102e264817aaf6ed2bb77971e2f6a144db93173
MHkif <malikhkif@gmail.com>
2024-02-15 15:07:26 +0100

    refactor main

--- src/main/java/mhkif/yc/myrh/config/security/SecurityConfig.java
+++ src/main/java/mhkif/yc/myrh/config/security/SecurityConfig.java
@@ -1,16 +1,21 @@
 package mhkif.yc.myrh.config.security;
 
+import jakarta.servlet.FilterRegistration;
 import mhkif.yc.myrh.config.security.jwt.JwtAuthenticationFilter;
 import mhkif.yc.myrh.service.UserDetailsServiceImpls.AdminUserDetails;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,32 +34,39 @@ import java.util.List;
 @AllArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(c -> c.disable());
-        http.authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(
-                                "myrh/api/v1/offers",
-                                "myrh/api/v1/admin/auth/*",
-                                "myrh/api/v1/companies/auth/"
-                        ).permitAll()
-                        .requestMatchers("myrh/api/v1/jobSeekers/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.GET,"myrh/api/v1/areaActivity/**","myrh/api/v1/profiles/**").permitAll()
-                        .requestMatchers("myrh/api/v1/companies/**").hasAuthority("COMPANY")
-                        .requestMatchers("myrh/api/v1/admin/**").hasAuthority("ADMIN")
-                        //.requestMatchers("myrh/api/v1/jobSeekers/**").hasAuthority("APPLICANT")
-                        .anyRequest().authenticated())
+        http
+                .csrf(AbstractHttpConfigurer::disable)
 
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors((cors) -> cors
                         .configurationSource(corsConfigurationSource())
-                );
+                )
 
 
-        http.addFilterAfter(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests(
+                        (authorize) -> authorize
+                                .requestMatchers(
+                                        "myrh/api/v1/login",
+                                        "myrh/api/v1/offers",
+                                        "myrh/api/v1/admin/auth/*",
+                                        "myrh/api/v1/companies/auth/",
+                                        "myrh/api/v1/jobSeekers/auth/*",
+                                        "http://localhost:8080/myrh-websocket"
+                                ).permitAll()
+                                .requestMatchers(HttpMethod.GET, "myrh/api/v1/areaActivity/**", "myrh/api/v1/profiles/**").permitAll()
+                                .requestMatchers("myrh/api/v1/companies/**").hasAuthority("COMPANY")
+                                .requestMatchers("myrh/api/v1/admin/**").hasAuthority("ADMIN")
+                                //.requestMatchers("myrh/api/v1/jobSeekers/**").hasAuthority("APPLICANT")
+                                .anyRequest().authenticated())
+
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterAfter(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
@@ -62,16 +74,24 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return  configuration.getAuthenticationManager();
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT));
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList(
+                "GET", "POST", "PUT", "DELETE", "PUT", "PATCH", "DELETE"));
+        configuration.setMaxAge(3600L);
+        //configuration.setExposedHeaders(List.of("Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+
         return source;
     }
 
